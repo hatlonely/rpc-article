0625c3208547455a74c5d3805777085315477e64
hatlonely <hatlonely@gmail.com>
2021-11-01 23:28:08 +0800

    update

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/hatlonely/rpc-article
 go 1.16
 
 require (
-	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/gogo/protobuf v1.3.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
 	github.com/hatlonely/go-kit v1.0.21
+	github.com/jinzhu/gorm v1.9.15
+	github.com/satori/go.uuid v1.2.0
+	github.com/smartystreets/goconvey v1.6.4
 	google.golang.org/genproto v0.0.0-20211016002631-37fc39342514
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
--- internal/storage/article/mysqldb_test.go
+++ internal/storage/article/mysqldb_test.go
@@ -1,7 +1,44 @@
 package article
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/hatlonely/go-kit/micro"
+
+	"github.com/hatlonely/go-kit/wrap"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNewMySQLDB(t *testing.T) {
+	Convey("TestNewMySQLDB", t, func() {
+		db, err := NewMySQLDB(&wrap.GORMDBWrapperOptions{
+			Retry: micro.RetryOptions{
+				Attempts:      1,
+				LastErrorOnly: true,
+			},
+			Wrapper: wrap.WrapperOptions{
+				Name: "articleDB",
+			},
+			Gorm: wrap.GormOptions{
+				Username:        "hatlonely",
+				Password:        "keaiduo1",
+				Database:        "article",
+				Host:            "127.0.0.1",
+				Port:            3306,
+				ConnMaxLifeTime: 60 * time.Second,
+				MaxIdleConns:    10,
+				MaxOpenConns:    20,
+				LogMode:         false,
+			},
+		})
+		So(err, ShouldBeNil)
+		So(db, ShouldNotBeNil)
+	})
+}
 
 func TestMySQLDB_PutArticle(t *testing.T) {
+	Convey("TestMySQLDB_PutArticle", t, func() {
 
+	})
 }
